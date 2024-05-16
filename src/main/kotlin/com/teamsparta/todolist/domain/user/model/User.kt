b9934f3335801db32f79ec6f0d43643d5c3a1381
pcouqc <nagapen@nate.com>
2024-05-16 15:58:02 +0900

    chore : remove unused anotation

--- src/main/kotlin/com/teamsparta/todolist/domain/user/model/User.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/user/model/User.kt
@@ -4,17 +4,13 @@ import com.teamsparta.todolist.domain.todos.model.Todos
 import jakarta.persistence.*
 
 
-@Entity
-@Table(name = "users")
+
+
 class User(
 
-    @Column(name = "name")
     val name : String,
 
-    @OneToMany(mappedBy = "user_id", fetch = FetchType.LAZY, cascade = [CascadeType.ALL], orphanRemoval = true)
-    var todos : MutableList<Todos> = mutableListOf()
 ) {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     var id : Long? = null
 }
