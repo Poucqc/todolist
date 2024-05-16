f7a1a1ce794c12c900142acf9d7163a589edcb77
pcouqc <nagapen@nate.com>
2024-05-16 15:50:11 +0900

    feat : Implement methods

--- src/main/kotlin/com/teamsparta/todolist/domain/todos/service/TodoServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todos/service/TodoServiceImpl.kt
@@ -1,42 +1,57 @@
 package com.teamsparta.todolist.domain.todos.service
 
+import com.teamsparta.todolist.domain.exception.TodoNotFoundException
 import com.teamsparta.todolist.domain.todos.dto.CreateTodoRequest
 import com.teamsparta.todolist.domain.todos.dto.TodoResponse
 import com.teamsparta.todolist.domain.todos.dto.UpdateTodoRequest
+import com.teamsparta.todolist.domain.todos.model.Todos
+import com.teamsparta.todolist.domain.todos.model.toResponse
+import com.teamsparta.todolist.domain.todos.repository.TodoRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 
 @Service
-class TodoServiceImpl: TodoService {
+class TodoServiceImpl(
+    private val todoRepository: TodoRepository
+) : TodoService {
+
     override fun getAllTodos(): List<TodoResponse> {
-        // TODO : 모든 TODO 를 가져와서 작성일 기준 내림차순으로 정렬 후 반환
-        TODO("Not yet implemented")
+        return todoRepository.findAllTodos().map{it.toResponse()}
     }
 
     override fun getTodoById(todoId: Long): TodoResponse {
-        // TODO : id 에 해당하는 todo 를 반환
-        // TODO : 해당하는 id 없을경우 throw TodoNotFoundException
-        TODO("Not yet implemented")
+        val todo = todoRepository.findByIdOrNull(todoId) ?: throw TodoNotFoundException("todo", todoId)
+        return todo.toResponse()
     }
 
     @Transactional
     override fun createTodo(request: CreateTodoRequest): TodoResponse {
-        // TODO : todo 생성 후 반환
-        TODO("Not yet implemented")
+        return todoRepository.save(
+            Todos(
+                author = request.author,
+                title = request.title,
+                content = request.content,
+                createdAt = LocalDateTime.now()
+            )
+        ).toResponse()
     }
 
     @Transactional
     override fun updateTodo(todoId: Long, request: UpdateTodoRequest): TodoResponse {
-        // TODO : id 에 해당하는 todo 를 수정 후 반환
-        // TODO : 해당하는 id 없을경우 throw TodoNotFoundException
-        TODO("Not yet implemented")
+        val todo = todoRepository.findByIdOrNull(todoId) ?: throw TodoNotFoundException("todo", todoId)
+        val (title, content) = request
+        todo.title = todo.title
+        todo.content = content
+
+        return todoRepository.save(todo).toResponse()
     }
 
     @Transactional
     override fun deleteTodo(todoId: Long) {
-        // TODO : id 에 해당하는 todo 삭제
-        // TODO : 해당하는 id 없을경우 throw TodoNotFoundException
-        TODO("Not yet implemented")
+        val todo = todoRepository.findByIdOrNull(todoId) ?: throw TodoNotFoundException("todo", todoId)
+        todoRepository.delete(todo)
     }
 }
