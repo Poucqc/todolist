32dbe1334f94225dad3acb3db5cf41d5c2be1382
pcouqc <nagapen@nate.com>
2024-05-16 15:31:47 +0900

    feat: change createdAt field type to LocalDateTime

--- src/main/kotlin/com/teamsparta/todolist/domain/todos/dto/TodoResponse.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todos/dto/TodoResponse.kt
@@ -1,10 +1,12 @@
 package com.teamsparta.todolist.domain.todos.dto
 
+import java.time.LocalDateTime
+
 data class TodoResponse(
     val id: Long,
     val title: String,
     val authorId: Long,
     val author: String,
     val content: String?,
-    val createdAt: Long,
+    val createdAt: LocalDateTime,
 )
