da2ff4df8c4ad94b6ab92e95626be25977b54b15
pcouqc <nagapen@nate.com>
2024-05-16 21:22:32 +0900

    feat : add dto types for comment package

---
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/dto/AddCommentRequest.kt
@@ -0,0 +1,7 @@
+package com.teamsparta.todolist.domain.comment.dto
+
+data class AddCommentRequest(
+    val author: String,
+    val password: String,
+    val comment: String
+)
---
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/dto/DeleteCommentRequest.kt
@@ -0,0 +1,5 @@
+package com.teamsparta.todolist.domain.comment.dto
+
+data class DeleteCommentRequest(
+    val password: String
+)
