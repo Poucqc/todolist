98c2d83c5543c53948d6de9e8426f4e54687b7a3
pcouqc <nagapen@nate.com>
2024-05-17 19:59:30 +0900

    feat : add new method for getTodoById method

--- src/main/kotlin/com/teamsparta/todolist/domain/todos/model/Todos.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todos/model/Todos.kt
@@ -1,9 +1,10 @@
 package com.teamsparta.todolist.domain.todos.model
 
+import com.teamsparta.todolist.domain.comment.dto.CommentResponse
 import com.teamsparta.todolist.domain.comment.model.Comment
 import com.teamsparta.todolist.domain.todos.dto.TodoResponse
+import com.teamsparta.todolist.domain.todos.dto.TodoWithCommentResponse
 import jakarta.persistence.*
-import org.hibernate.annotations.Comments
 import java.time.LocalDateTime
 
 
@@ -24,7 +25,7 @@ class Todos(
     var createdAt: LocalDateTime,
 
     @OneToMany(cascade = [(CascadeType.ALL)], fetch = FetchType.EAGER, orphanRemoval = true)
-    var comments : MutableList<Comment> = mutableListOf()
+    var comments: MutableList<Comment> = mutableListOf()
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,3 +41,15 @@ fun Todos.toResponse(): TodoResponse {
         createdAt = createdAt
     )
 }
+
+fun Todos.toResponseWithComments(comments: List<Comment>?): TodoWithCommentResponse {
+    return TodoWithCommentResponse(
+        id = id!!,
+        title = title,
+        author = author,
+        content = content,
+        createdAt = createdAt,
+        comments = comments?.map{ CommentResponse(it.author, it.comment) }
+    )
+}
+
