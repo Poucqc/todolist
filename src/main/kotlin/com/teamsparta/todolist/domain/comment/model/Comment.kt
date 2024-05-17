e7c5fdf68935718d89f786bd8b1d9244359a877d
pcouqc <nagapen@nate.com>
2024-05-17 11:59:12 +0900

    feat: establish Entity relationships and map tables using annotations and add utility function for Response

--- src/main/kotlin/com/teamsparta/todolist/domain/comment/model/Comment.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/model/Comment.kt
@@ -1,19 +1,35 @@
 package com.teamsparta.todolist.domain.comment.model
 
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import com.teamsparta.todolist.domain.comment.dto.CommentResponse
+import com.teamsparta.todolist.domain.todos.model.Todos
+import jakarta.persistence.*
 
+@Entity
+@Table(name = "comment")
 class Comment(
 
+    @Column(name = "author", nullable = false)
     val author: String,
 
+    @Column(name = "password", nullable = false)
     val password: String,
 
-    val comment: String
+    @Column(name = "comment", nullable = false)
+    val comment: String,
 
+    @ManyToOne(cascade = [(CascadeType.MERGE)], fetch = FetchType.LAZY)
+    @JoinColumn(name = "todoId", nullable = false)
+    var todo : Todos
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
+}
+
+fun Comment.Response (): CommentResponse {
+    return CommentResponse(
+        id = id!!,
+        author = author,
+        comment = comment
+    )
 }
