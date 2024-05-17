7471b4c697446e487af54485e00d19073efa7fb1
pcouqc <nagapen@nate.com>
2024-05-17 11:26:55 +0900

    refactor : Rename ExceptionHandler (ModelNotFoundException, GlobalExceptionHandler)

--- src/main/kotlin/com/teamsparta/todolist/domain/exception/dto/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/exception/dto/GlobalExceptionHandler.kt
@@ -1,6 +1,6 @@
 package com.teamsparta.todolist.domain.exception.dto
 
-import com.teamsparta.todolist.domain.exception.TodoNotFoundException
+import com.teamsparta.todolist.domain.exception.ModelNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
-    @ExceptionHandler(TodoNotFoundException::class)
-    fun handleTodoNotFoundException(e: TodoNotFoundException): ResponseEntity<ErrorResponse> {
+    @ExceptionHandler(ModelNotFoundException::class)
+    fun handleTodoNotFoundException(e: ModelNotFoundException): ResponseEntity<ErrorResponse> {
         return ResponseEntity
             .status(HttpStatus.NOT_FOUND)
             .body(ErrorResponse(e.message))
