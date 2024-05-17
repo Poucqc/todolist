7a204b94f2dd22d7e322369a4daa4941b39fe7ad
pcouqc <nagapen@nate.com>
2024-05-17 11:27:29 +0900

    chore : remove unused class

--- src/main/kotlin/com/teamsparta/todolist/domain/exception/TodoNotFoundException.kt
+++
@@ -1,6 +0,0 @@
-package com.teamsparta.todolist.domain.exception
-
-data class TodoNotFoundException(
-    val modelName: String,
-    val id: Long?
-) : RuntimeException("Model $modelName not found given id : $id")
