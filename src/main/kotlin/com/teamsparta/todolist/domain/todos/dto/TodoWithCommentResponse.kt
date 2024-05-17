0e9873cf3beb8f0136a4999678b4a289eb16259d
pcouqc <nagapen@nate.com>
2024-05-17 19:57:54 +0900

    feat : Add Dto TodoWithCommentResponse

---
+++ src/main/kotlin/com/teamsparta/todolist/domain/todos/dto/TodoWithCommentResponse.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.todolist.domain.todos.dto
+
+import com.teamsparta.todolist.domain.comment.dto.CommentResponse
+import java.time.LocalDateTime
+
+data class TodoWithCommentResponse(
+    val id: Long,
+    val title: String,
+    val author: String,
+    val content: String?,
+    val createdAt: LocalDateTime,
+    val comments: List<CommentResponse>?
+)
