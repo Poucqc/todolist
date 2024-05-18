b312a8c805202bf669d86c875739afe7af320769
pcouqc <nagapen@nate.com>
2024-05-18 16:01:14 +0900

    feat : 동작 구현 - addComment : todo 조회후 해당 todo 에 comment 작성, - updateComment : 작성한 comment 수정, - deleteComment : 작성한 comment 삭제, - validateCommentAccess : 해당 comment 에 대한 id 및 password 검증

--- src/main/kotlin/com/teamsparta/todolist/domain/comment/service/CommentServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/service/CommentServiceImpl.kt
@@ -4,30 +4,58 @@ import com.teamsparta.todolist.domain.comment.dto.AddCommentRequest
 import com.teamsparta.todolist.domain.comment.dto.CommentResponse
 import com.teamsparta.todolist.domain.comment.dto.DeleteCommentRequest
 import com.teamsparta.todolist.domain.comment.dto.UpdateCommentRequest
+import com.teamsparta.todolist.domain.comment.model.Comment
+import com.teamsparta.todolist.domain.comment.model.toCommentResponse
+import com.teamsparta.todolist.domain.comment.repository.CommentRepository
+import com.teamsparta.todolist.domain.exception.ModelNotFoundException
+import com.teamsparta.todolist.domain.exception.PasswordNotMatchedException
+import com.teamsparta.todolist.domain.todos.model.Todos
+import com.teamsparta.todolist.domain.todos.repository.TodoRepository
+import com.teamsparta.todolist.security.PasswordManager
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class CommentServiceImpl: CommentService {
+class CommentServiceImpl(
+    private val todoRepository: TodoRepository,
+    private val commentRepository: CommentRepository,
+    private val passwordManager: PasswordManager
+) : CommentService {
+
 
     @Transactional
     override fun addComment(todoId: Long, request: AddCommentRequest): CommentResponse {
-        // TODO : 해당 id Todo 에 comment 를 작성
-        // todo : todo 가 존재하지 않으면 exception : todo not found
-        TODO("Not yet implemented")
+        val todo = todoRepository.findByIdOrNull(todoId) ?: throw ModelNotFoundException("todo", todoId)
+        return commentRepository.save(
+            Comment(
+                author = request.author,
+                password = request.password,
+                commentContent = request.comment,
+                todo = todo
+            )
+        ).toCommentResponse()
     }
 
     @Transactional
     override fun updateComment(todoId: Long, commentId: Long, request: UpdateCommentRequest): CommentResponse {
-        // todo : 해당 todoId 의 todo 에서 해당하는 commentId 의 comment 를 수정
-        // todo : todoId 혹은 commentId 존재하지 않으면 ModelNotFoundException(todo or comment , id)
-        TODO("Not yet implemented")
+        val (todos, comment) = validateCommentAccess(todoId, commentId, request.password)
+        val commentContent = request.comment
+        return commentRepository.save(comment).toCommentResponse()
     }
 
     @Transactional
     override fun deleteComment(todoId: Long, commentId: Long, request: DeleteCommentRequest) {
-        // todo : 해당 todoId 의 todo 에서 해당하는 commentId 의 comment 를 삭제
-        // todo : todoId 혹은 commentId 존재하지 않으면 ModelNotFoundException(todo or comment , id)
-        TODO("Not yet implemented")
+        val (todos, comment) = validateCommentAccess(todoId, commentId, request.password)
+        commentRepository.delete(comment)
+    }
+
+    private fun validateCommentAccess(todoId: Long, commentId: Long, password: String): Pair<Todos, Comment> {
+        val todo = todoRepository.findByIdOrNull(todoId) ?: throw ModelNotFoundException("todo", todoId)
+        val comment = commentRepository.findByIdOrNull(commentId) ?: throw ModelNotFoundException("comment", commentId)
+        if (!passwordManager.isPasswordValid(comment.password, password)) {
+            throw PasswordNotMatchedException(commentId)
+        }
+        return Pair(todo, comment)
     }
 }
