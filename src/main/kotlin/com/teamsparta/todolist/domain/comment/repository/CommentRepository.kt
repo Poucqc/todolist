d7297be5381cbbbac71cb8697be0a41f389e1eba
pcouqc <nagapen@nate.com>
2024-05-20 20:24:16 +0900

    feat : add @Repository

--- src/main/kotlin/com/teamsparta/todolist/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/repository/CommentRepository.kt
@@ -2,7 +2,9 @@ package com.teamsparta.todolist.domain.comment.repository
 
 import com.teamsparta.todolist.domain.comment.model.Comment
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface CommentRepository: JpaRepository<Comment, Long> {
 
     fun findCommentByTodoId(id: Long): List<Comment>?
