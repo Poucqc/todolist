3e06fa937738bc70f8e5cf1ae6e678a6bccaf5d3
pcouqc <nagapen@nate.com>
2024-05-22 15:54:08 +0900

    refactor : Remove unused import

--- src/main/kotlin/com/teamsparta/todolist/domain/todos/repository/TodoRepository.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todos/repository/TodoRepository.kt
@@ -1,11 +1,8 @@
 package com.teamsparta.todolist.domain.todos.repository
 
-import com.teamsparta.todolist.domain.todos.model.OrderType
 import com.teamsparta.todolist.domain.todos.model.Todos
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import java.time.LocalDateTime
 
 @Repository
 interface TodoRepository: JpaRepository<Todos, Long> {
