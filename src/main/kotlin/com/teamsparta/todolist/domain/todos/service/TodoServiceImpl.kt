09bee07b7813a9c87c24f49eaf20f533e288df65
pcouqc <nagapen@nate.com>
2024-05-24 01:21:55 +0900

    refactor : remove unused import

--- src/main/kotlin/com/teamsparta/todolist/domain/todos/service/TodoServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todos/service/TodoServiceImpl.kt
@@ -13,7 +13,6 @@ import com.teamsparta.todolist.domain.todos.repository.TodoRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
