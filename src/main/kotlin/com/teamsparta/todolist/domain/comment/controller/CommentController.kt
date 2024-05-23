51c34955380099a0d7dad9ad37b6ff71feb4ecbb
pcouqc <nagapen@nate.com>
2024-05-24 00:54:15 +0900

    refactor : change endpoint for clarify

--- src/main/kotlin/com/teamsparta/todolist/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/controller/CommentController.kt
@@ -5,7 +5,6 @@ import com.teamsparta.todolist.domain.comment.dto.CommentResponse
 import com.teamsparta.todolist.domain.comment.dto.DeleteCommentRequest
 import com.teamsparta.todolist.domain.comment.dto.UpdateCommentRequest
 import com.teamsparta.todolist.domain.comment.service.CommentService
-import io.swagger.v3.oas.annotations.parameters.RequestBody
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -27,7 +26,7 @@ class CommentController(
             .body(commentService.addComment(todoId, addCommentRequest))
     }
 
-    @PutMapping("/{commentId}")
+    @PutMapping("/update/{commentId}")
     fun updateComment(
         @PathVariable("todoId") todoId: Long,
         @PathVariable("commentId") commentId: Long,
@@ -38,7 +37,7 @@ class CommentController(
             .body(commentService.updateComment(todoId, commentId, updateCommentRequest))
     }
 
-    @DeleteMapping("/{commentId}")
+    @DeleteMapping("/delete/{commentId}")
     fun deleteComment(
         @PathVariable("todoId") todoId: Long,
         @PathVariable("commentId") commentId: Long,
