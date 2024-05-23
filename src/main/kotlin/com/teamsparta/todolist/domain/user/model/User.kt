97be4be741d458394ee44e5aec0b82015246c25a
pcouqc <nagapen@nate.com>
2024-05-24 00:51:30 +0900

    build : add user packages

--- src/main/kotlin/com/teamsparta/todolist/domain/user/model/User.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/user/model/User.kt
@@ -1,16 +1,31 @@
 package com.teamsparta.todolist.domain.user.model
 
 import com.teamsparta.todolist.domain.todos.model.Todos
+import com.teamsparta.todolist.domain.user.dto.UserResponse
 import jakarta.persistence.*
 
 
 
-
+@Entity
+@Table(name = "users")
 class User(
-
+    @Column(name = "user_name", nullable = false)
     val name : String,
 
-) {
+    @Column(name = "password", nullable = false)
+    var password : String,
 
+    @OneToMany(cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY, orphanRemoval = true)
+    val todos : MutableList<Todos>? = mutableListOf()
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id : Long? = null
+}
+
+fun User.toResponse() : UserResponse {
+    return UserResponse(
+        id = id!!,
+        name = name,
+    )
 }
