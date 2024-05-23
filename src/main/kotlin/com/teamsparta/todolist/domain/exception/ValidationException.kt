5c137044746fa95944e319593b1d729c8093f03d
pcouqc <nagapen@nate.com>
2024-05-24 00:50:34 +0900

    refactor : remove unused classes

--- src/main/kotlin/com/teamsparta/todolist/domain/exception/ValidationException.kt
+++
@@ -1,6 +0,0 @@
-package com.teamsparta.todolist.domain.exception
-
-data class ValidationException(
-    val modelName: String,
-    val fieldName: String
-) : RuntimeException("model $modelName's $fieldName validation failed, please check form")
